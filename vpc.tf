0f43767f421b8c7997ddc5ed7c447e6fa42f23ad
jecominfo <info@jecom.ca>
2020-09-15 23:02:37 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -6,9 +6,10 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = "us-east-1"
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   
-
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
